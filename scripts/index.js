6217c9870bd715b6b45cc4a8c25a10a00f099650
Facundo Erik <florfacundoerik@gmail.com>
2025-06-20 14:41:23 -0300

    FEAT: GBE-39 | GBE-43 - Se generan vistas para analista operaciones

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 import { runAllModules } from "./wrapperCalls.js";
+import { operationsAnalystView } from "./views/views.js";
 
 
 document.addEventListener("DOMContentLoaded", async () => {
     await runAllModules();
 
-
+    operationsAnalystView();
 })
